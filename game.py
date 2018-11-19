b7153a460ef051d38e499e54b7e03927eb8a989b
DollaR <cooler84@ukr.net>
2018-11-19 23:34:05 +0200

    add change language

--- game.py
+++ game.py
@@ -102,6 +102,8 @@ class Game:
                     self.change_deck()
                 elif pygame.K_F5 == event.key:
                     self.new_game()
+                elif pygame.K_F9 == event.key:
+                    self.change_language()
                 elif pygame.K_TAB == event.key and pygame.key.get_mods() & pygame.KMOD_SHIFT:
                     if not self.game_over:
                         self.player.actions(Actions.ChangeZoneDown)
@@ -212,5 +214,20 @@ class Game:
         else:
             self.config.set('board', 'deck', 'half')
             self.speech.speak(self.phrases['36_cards'])
+        with open('settings.ini', 'w') as config_file:
+            self.config.write(config_file)
+
+    def change_language(self):
+        """Change language for phrases."""
+        if 'ru' == self.config.get('total', 'language'):
+            self.config.set('total', 'language', 'en')
+            with open('languages.dat', 'rb') as lang_file:
+                self.phrases = pickle.load(lang_file)['en']
+        else:
+            self.config.set('total', 'language', 'ru')
+            with open('languages.dat', 'rb') as lang_file:
+                self.phrases = pickle.load(lang_file)['ru']
+        self.player.phrases = self.phrases
+        self.speech.speak(self.phrases['language'])
         with open('settings.ini', 'w') as config_file:
             self.config.write(config_file)
--- readme_en.txt
+++ readme_en.txt
@@ -10,6 +10,7 @@ F2 - on/off music;
 F3 - change level;
 F4 - change deck 36 or 52 cards;
 F5 - new game;
+F9 - change language;
 Tab - change zone;
 Shift+Tab - change zone reverse;
 Space - take card or put if card took;
