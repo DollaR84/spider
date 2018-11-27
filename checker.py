618a9d72cb521574694190a787efe84e0a4250d8
DollaR <cooler84@ukr.net>
2018-11-27 18:52:56 +0200

    add function for check equal suits in cards list

--- checker.py
+++ checker.py
@@ -10,7 +10,7 @@ Created on 26.10.2018
 from constants import Cards
 
 
-def change_suit(card1, card2):
+def change_color_suit(card1, card2):
     """Check change suit two cards."""
     if card1.suit in Cards.black_suits:
         return True if card2.suit in Cards.red_suits else False
@@ -18,18 +18,27 @@ def change_suit(card1, card2):
         return True if card2.suit in Cards.black_suits else False
 
 
-def change_suits(cards):
+def equal_suit(card1, card2):
+    """Check equal suit two cards."""
+    return True if card1.suit == card2.suit else False
+
+
+def change_color_suits(cards):
     """Check change suits cards."""
     for index, card in enumerate(cards):
         if (len(cards) - 1) > index:
-            if not change_suit(card, cards[index + 1]):
+            if not change_color_suit(card, cards[index + 1]):
                 return False
     return True
 
 
 def equal_suits(cards):
     """Check equal suits cards."""
-    return not change_suits(cards)
+    for index, card in enumerate(cards):
+        if (len(cards) - 1) > index:
+            if not equal_suit(card, cards[index + 1]):
+                return False
+    return True
 
 
 def rate_down(cards):
